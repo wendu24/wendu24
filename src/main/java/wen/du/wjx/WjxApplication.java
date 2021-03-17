d881c385d7d9d578f32880a860d09b288abffd3e
wendu24 <wendu2424@gmail.com>
2021-03-17 22:30:40 +0800

    first

---
+++ src/main/java/wen/du/wjx/WjxApplication.java
@@ -0,0 +1,13 @@
+package wen.du.wjx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjxApplication.class, args);
+    }
+
+}
